b2dc2dd003f89aba3c2a3edfad674c82d4f2c3c1
Ilya Prikhodko <zapaska96@gmail.com>
2024-06-11 20:35:28 +0500

    two-way binding

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    Text("Hello, world!")
+                    TextField("Name here", text: $name)
+                    Text("Your name is \(name)")
                 }
-                
                 Button("Tap count: \(tapCount)") {
                     self.tapCount += 1
                 }
